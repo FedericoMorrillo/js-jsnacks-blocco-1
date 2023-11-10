66b500a8edcf378ecba9774083753cb85814bf24
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-10 11:25:48 +0100

    fine esercizio1

---
+++ snack-1/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//dichiarazione variabili
+const parola1 = prompt("inserisci una parola");
+const parola2 = prompt("inserisci un altra parola");
+
+// condizioni    
+if(parola1.length > parola2.length){
+    console.log(parola1+" è più lunga di " + parola2);
+}
+else if(parola2.length > parola1.length){
+    console.log(parola2+" è più lunga di " + parola1);
+}
+else{
+    console.log(" le parole "+ parola1 + " e "+ parola2 + " hanno la stessa lunghezza" )
+}
