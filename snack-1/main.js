393cc86ce96baca6fc1bc0907a1204bb6a23dc5b
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-10 11:38:59 +0100

    refactoring

--- snack-1/main.js
+++ snack-1/main.js
@@ -6,10 +6,12 @@ const parola2 = prompt("inserisci un altra parola");
 
 // condizioni    
 if(parola1.length > parola2.length){
-    console.log(parola1+" è più lunga di " + parola2);
+    console.log(parola1+" è più lunga di:");
+    console.log(parola2);
 }
 else if(parola2.length > parola1.length){
-    console.log(parola2+" è più lunga di " + parola1);
+    console.log(parola2+" è più lunga di:");
+    console.log(parola1)
 }
 else{
     console.log(" le parole "+ parola1 + " e "+ parola2 + " hanno la stessa lunghezza" )
