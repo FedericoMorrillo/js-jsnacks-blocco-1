40bbfd8e2f74411cdb00a6a49120c7b3e09fe252
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-10 12:02:41 +0100

    feat: fine esercizio 2

---
+++ snack-2/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// 10 input numerici inseriti dall' utente
+const numero1 = Number(prompt("inserisci un numero"));
+const numero2 = Number(prompt("inserisci un numero"));
+const numero3 = Number(prompt("inserisci un numero"));
+const numero4 = Number(prompt("inserisci un numero"));
+const numero5 = Number(prompt("inserisci un numero"));
+const numero6 = Number(prompt("inserisci un numero"));
+const numero7 = Number(prompt("inserisci un numero"));
+const numero8 = Number(prompt("inserisci un numero"));
+const numero9 = Number(prompt("inserisci un numero"));
+const numero10 = Number(prompt("inserisci un numero"));
+
+// somma dei valori
+const risultato = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10;
+
+//stampa del risultato
+console.log(`il risultato di: ${numero1} + ${numero2} + ${numero3} + ${numero4} + ${numero5} + +${numero6} + ${numero7} + ${numero8} + ${numero9} + ${numero10} è uguale a =`)
+console.log(risultato)
